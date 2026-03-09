b9f2d5974f932193253fa0f99b84a89e9aabe1b1
Cédric Matyja <c.matyja@gmail.com>
2026-03-09 22:18:15 +0100

    feat(keystatic): mise à jour config `astro.config.mjs`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,22 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import mdx from "@astrojs/mdx";
+import keystatic from "@keystatic/astro";
+
+const isDevMode = import.meta.env.DEV;
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cajytam.com",
+  site: "https://cajytam.fr",
+  output: "static",
   integrations: [
     icon({
       include: {
         "simple-icons": ["*"],
       },
     }),
+    mdx(),
+    ...(isDevMode ? [keystatic()] : []),
   ],
 });
